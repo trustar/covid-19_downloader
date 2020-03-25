bb7c0fa692ad42cd188825a7c8f83ef0fd070394
Steve Chamales <schamales@trustar.co>
2020-03-25 09:40:20 -0700

    added command to create the 'config_file/private' directory.

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+mkdir config_file/private
 cp config_file/example/trustar.conf.spec config_file/private/
